6b9c156fadb8993a81e129b4dedebb3be0c00101
Eiisley <102468937+Eiisley@users.noreply.github.com>
2022-03-28 10:39:51 +0800

    Update index.php
    with slight design

--- index.php
+++ index.php
@@ -4,13 +4,93 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BicyClean+ | Reservation Page</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+    <title>BicyClean+ | Reservation Page |Bicycle Services Reservation System</title>
 </head>
+
+<style>
+    html, body{
+      display: flex;
+      justify-content: center;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 15px;
+      background-color: #e6ccb2;
+      }
+
+      form {
+      background-color: #fff;
+  padding: 2rem 3rem 4rem;
+  box-shadow: 2px 2px 16px 3px rgba(0, 0, 0, 0.4);
+      color: #f2cc8f;
+      background-color: #3d405b;
+      border-radius: 8px;
+      }
+
+      input[type=text], input[type=number] {
+      width: 100%;
+      padding: 16px 8px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      }
+
+       input[type=submit]{
+        color: white;
+         font-family: Roboto, Arial, sans-serif;
+      font-size: 15px;
+       }
+
+      button {
+      background-color: beige;
+      color: white;
+      padding: 14px 0;
+      margin: 10px 0;
+    border-radius: 8px;
+      cursor: grabbing;
+      width: 100%;
+      }
+
+      h1 {
+      text-align:center;
+      font-size:22px;
+      }
+      button:hover {
+      opacity: 0.8;
+      }
+      .formcontainer {
+      text-align: left;
+      margin: 24px 50px 12px;
+      }
+      .container {
+      padding: 16px 0;
+      text-align:left;
+      }
+      span.psw {
+      float: right;
+      padding-top: 0;
+      padding-right: 15px;
+      }
+      /* Change styles for span on extra small screens */
+      @media screen and (max-width: 300px) {
+      span.psw {
+      display: block;
+      float: none;
+      }
+
+</style>
+
+
+
 <body>
-    <h2>Bicyclean+: Bicycle Services Reservation System</h2>
-    <h3>Reservation Page</h3>
+       
 
     <form action="controller/process-reservation.php" method="post">
+    <h1 style="text-align: center;">Bicyclean+: Bicycle Services Reservation </h2>
+    <h1 style="text-align: center;">Reservation Form</h3>
+
         <h3>Full Name</h3>
         <input type="text" placeholder="Enter your Full Name" required>
         <h3>Email Address</h3>
@@ -19,7 +99,7 @@
         <input type="number" placeholder="Enter your phone number (+63)" required>
         <h3>Service Type</h3>
         <label for="service-type"></label>
-        <select name="service-type" id="service-type">
+        <select style ="border-radius: 4px;"name="service-type" id="service-type">
             <option value="1">Wheel Truing with Tensioning Application</option>
             <option value="2">Wheel build with tension application</option>
             <option value="3">Install Bike Rack</option>
@@ -27,20 +107,23 @@
             <option value="5">Basic Suspension Service</option>
         </select>
         <br>
+        <br>
        
         <textarea name="notes" id="" cols="30" rows="10" placeholder="Please mention your additional requests here. Type N/A if none." ></textarea>
         <h3>Date</h3>
         <?php 
          $mindate = date("Y-m-d");
         ?>
-         <input type="date" required id="res_date" name="date" min="<?=$mindate?>">
+         <input type="date" style ="border-radius: 4px;" required id="res_date" name="date" min="<?=$mindate?>">
          <br>
         <label for="timeslot"> <h3>Select Time Slot (AM or PM only)</h3></label>
-        <select name="timeslot" id="timeslot">
+        <select name="timeslot" style ="border-radius: 4px;"id="timeslot">
             <option value="AM">AM</option>
             <option value="PM">PM</option>
         </select>
-        <input type="submit" name="submit">
+        <br>
+        <br>
+        <button><input type="submit" style = "background: none; border: none; color: dimgrey;"name="submit"></button>
     </form>
 </body>
 </html>
